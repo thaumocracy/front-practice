d3e961fbc6350b4c59ab5fd5c019464fb413d626
Winterstorm <thunpwnz@gmail.com>
2019-02-04 16:30:43 +0400

    Particles everywhere!

--- Myself/react/ztmfaceapp/src/App.js
+++ Myself/react/ztmfaceapp/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
+import Particles from 'react-particles-js'
 import './App.css'
 import 'tachyons'
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
+import Rank from './components/Rank/Rank'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import particlesOptions from './particlesOptions';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles 
+          className="particles"
+          params={particlesOptions} 
+        />
         <Navigation />
         <Logo />
+        <Rank />
+        <ImageLinkForm />
       </div>
     );
   }
