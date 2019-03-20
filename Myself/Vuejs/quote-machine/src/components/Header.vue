1df3f8a56ed5242ae639d8b7918f80a967f3b288
Winterstorm <thunpwnz@gmail.com>
2019-03-20 14:46:15 +0400

    CSS updates

--- Myself/Vuejs/quote-machine/src/components/Header.vue
+++ Myself/Vuejs/quote-machine/src/components/Header.vue
@@ -13,7 +13,7 @@ export default {
 <style scoped>
 .main-header {
     height:10vh;
-    background: linear-gradient(to left, #042643, #434343);
+    background: rgb(38, 0, 0);
     width:100%;
     margin:0;
     padding:0;
@@ -22,7 +22,7 @@ export default {
 .main-header-text {
     text-align: center;
     font-size:3em;
-    color:#FEF9C4;
+    color:#fff2f2;
     margin:0;
     padding:20px 10px;
     letter-spacing: 0.2em;
--- Myself/Vuejs/quote-machine/src/components/Main.vue
+++ Myself/Vuejs/quote-machine/src/components/Main.vue
@@ -4,14 +4,14 @@
             <h3 class="quote-first">{{first}}</h3>
             <img :src="picture" alt="Some Picture">
             <h3 class="quote-second">{{second}}</h3>
-            <button class="destroy" @click="findPicture">KILL IT WITH FIRE</button>
+            <button class="destroy" @click="findPicture">ASK SOMEONE</button>
         </div>
 
     </main>
 </template>
 
 <script>
-const  picArray = ['brezhnev','brezhnev2','buzova','buzova2','einstein4','einstein5','lenin','lenin2','show','snow2','statham']
+const  picArray = ['brezhnev','brezhnev2','buzova','buzova2','einstein4','einstein5','lenin','lenin2','snow','snow2','statham']
 import quoteArray from '../assets/quotes.json'
 
 
@@ -38,12 +38,13 @@ export default {
 
 <style scoped>
 .content {
-    background: linear-gradient(to right, #000000, #434343);
+    /* background: linear-gradient(to right, #000000, #434343); */
     height:90vh;
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
     align-items: center;
+    background: linear-gradient(to right, rgba(46,6,15,1) 0%, rgba(143,2,34,1) 44%, rgba(132,2,31,1) 49%, rgba(20,7,1,1) 100%);
 }
 img {
     display:block;
@@ -57,30 +58,36 @@ img {
     width:40%;
     margin:0 auto;
     font-size:2em;
-    color:wheat;
+    color:#fff2f2;
+
 }
 .quote-first {
     text-align: start;
     padding:20px;
+    text-shadow: 2px 2px  #000;
 }   
 .quote-second {
     text-align: end;
     padding:20px;
+    text-shadow: 2px 2px  #000;
 }
 .destroy {
     margin-top:20px;
     display: block;
     width:100%;
     padding:20px;
-    color:thistle;
+    color:#fff2f2;
     font-size:2em;
     border:none;
     background: linear-gradient(to right, #f12711, #f5af19);
+    text-shadow: 2px 2px  #000;
 }
 
 .destroy:hover {
     outline: 5px solid red;
     color:black;
+    box-shadow: 6px 22px 53px -26px rgba(255,255,255,1);
+    text-shadow: 2px 2px  white;
 }
 
 .destroy:focus {
