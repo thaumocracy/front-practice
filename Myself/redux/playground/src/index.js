6ddc83d8c0019c126956ff2e8b26c8e3a78b5072
Winterstorm <thunpwnz@gmail.com>
2019-04-08 21:30:54 +0400

    Redux again

--- Myself/redux/playground/src/index.js
+++ Myself/redux/playground/src/index.js
@@ -5,16 +5,18 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+// import { Provider } from 'react-redux'
+let album = {
+  title: 'Kind of Blue',
+  artist: 'Miles Davis',
+  year: 1959
+}
+const reducer = {}
+const store = createStore(reducer,album)
+let state = store.getState()
+console.log(store.title)
 
-const store = createStore(reducer)
-
-ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>
-
-, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
