55b10fdcb65cf3544b34230cf0bbbe3299e2c53b
Winterstorm <thunpwnz@gmail.com>
2019-04-19 23:46:58 +0400

    Routing

--- Myself/react/routestry/src/containers/Blog/Blog.js
+++ Myself/react/routestry/src/containers/Blog/Blog.js
@@ -50,7 +50,15 @@ class Blog extends Component {
         }
 
         return (
-            <div>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/" target="_blank" rel="noopener noreferrer">Home</a></li>
+                            <li><a href="/new-post" target="_blank" rel="noopener noreferrer">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
                 </section>
