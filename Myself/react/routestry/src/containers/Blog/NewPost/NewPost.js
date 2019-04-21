de4c8a6b9d0982c8b31bff6ccbf49f4ceb985b92
Winterstorm <thunpwnz@gmail.com>
2019-04-21 23:46:39 +0400

    Routing : Update

--- Myself/react/routestry/src/containers/Blog/NewPost/NewPost.js
+++ Myself/react/routestry/src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Redirect} from 'react-router-dom'
 
 import './NewPost.css';
 
@@ -7,7 +8,8 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Thaumocracy',
+        submitted:false
     }
 
     postDataHandler = () => {
@@ -19,12 +21,18 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(response => {
                 console.log(response);
+                this.setState({submitted:true})
             });
     }
 
     render () {
+        let redirect = null
+        if(this.state.submitted){
+            redirect = <Redirect to="/posts"/>
+        }
         return (
             <div className="NewPost">
+            {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
@@ -32,8 +40,8 @@ class NewPost extends Component {
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+                    <option value="Thaumocracy">Thaumocracy</option>
                     <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
                 </select>
                 <button onClick={this.postDataHandler}>Add Post</button>
             </div>
