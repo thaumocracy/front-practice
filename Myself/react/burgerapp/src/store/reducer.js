9d92ac17cc5398cfd303d5b1b79ab3efabb36bbe
Winterstorm <thunpwnz@gmail.com>
2019-04-30 17:38:00 +0400

    BurgerApp : Redux initial setup

---
+++ Myself/react/burgerapp/src/store/reducer.js
@@ -0,0 +1,15 @@
+import * as constants from './actions'
+
+const initialState = {
+  ingredients:null,
+  totalPrice:4
+}
+
+const reducer = (state=initialState,action) => {
+  switch(action.type){
+    default : 
+      return state
+  }
+}
+
+export default reducer
