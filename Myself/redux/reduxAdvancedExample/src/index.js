5874188df0cd51a2daae1baa5668b251c393ae06
Winterstorm <thunpwnz@gmail.com>
2019-05-05 00:00:21 +0400

    Redux : Advanced DevTools connected

--- Myself/redux/reduxAdvancedExample/src/index.js
+++ Myself/redux/reduxAdvancedExample/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers , applyMiddleware  , compose } from 'redux';
 
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
@@ -14,7 +14,19 @@ const rootReducer = combineReducers({
     res: resultReducer
 });
 
-const store = createStore(rootReducer);
+const logger = (store) => {
+    return next => {
+        return action => {
+            console.log('[MIDDLEWARE] DISPATCHING', action)
+            const result = next(action)
+            return result
+        }
+    }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer,composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
