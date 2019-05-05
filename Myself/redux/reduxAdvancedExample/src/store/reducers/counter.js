ea920cc86cbfbb02ba52ab86da89c7e3574a46bb
Winterstorm <thunpwnz@gmail.com>
2019-05-05 18:55:32 +0400

    Redux : Middleware and actionCreators

--- Myself/redux/reduxAdvancedExample/src/store/reducers/counter.js
+++ Myself/redux/reduxAdvancedExample/src/store/reducers/counter.js
@@ -1,4 +1,4 @@
-import * as actionTypes from '../actions';
+import * as actionTypes from '../actions/actions';
 
 const initialState = {
     counter: 0
@@ -18,12 +18,12 @@ const reducer = ( state = initialState, action ) => {
         case actionTypes.ADD:
             return {
                 ...state,
-                counter: state.counter + action.val
+                counter: state.counter + action.value
             }
         case actionTypes.SUBTRACT:
             return {
                 ...state,
-                counter: state.counter - action.val
+                counter: state.counter - action.value
             }
     }
     return state;
