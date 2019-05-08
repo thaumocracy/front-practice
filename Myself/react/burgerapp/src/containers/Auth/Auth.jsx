7f2107c08a55bf041ab09fc6afe13706404f33d2
Winterstorm <thunpwnz@gmail.com>
2019-05-08 23:55:54 +0400

    Auth : Firebase auth and forms

--- Myself/react/burgerapp/src/containers/Auth/Auth.jsx
+++ Myself/react/burgerapp/src/containers/Auth/Auth.jsx
@@ -38,23 +38,28 @@ class Auth extends Component {
         valid:false,
         touched:false,
       },
-    }
+    },
+    isSignIn: true,
   }
 
-  
-    checkValidity(value, rules) {
-      let isValid = true;
-      if (!rules) {return true}      
-      if (rules.required) {isValid = value.trim() !== '' && isValid;}
-      if (rules.minLength) {isValid = value.length >= rules.minLength && isValid}
-      if (rules.maxLength) {isValid = value.length <= rules.maxLength && isValid}
-      if (rules.isEmail) {
-          const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-          isValid = pattern.test(value) && isValid
-      }
-      if (rules.isNumeric) {const pattern = /^\d+$/;isValid = pattern.test(value) && isValid}
-      return isValid;
-  }
+  switchAuthModeHandler = () => {
+    this.setState(prevState => {
+      return {isSignIn:!prevState.isSignIn}
+    })}
+
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (!rules) {return true}      
+    if (rules.required) {isValid = value.trim() !== '' && isValid;}
+    if (rules.minLength) {isValid = value.length >= rules.minLength && isValid}
+    if (rules.maxLength) {isValid = value.length <= rules.maxLength && isValid}
+    if (rules.isEmail) {
+        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        isValid = pattern.test(value) && isValid
+    }
+    if (rules.isNumeric) {const pattern = /^\d+$/;isValid = pattern.test(value) && isValid}
+    return isValid;
+ }
 
 
   inputChangedHandler = (event,controlName) => {
@@ -72,7 +77,7 @@ class Auth extends Component {
 
   submitHandler = (event) => {
     event.preventDefault()
-    this.props.onAuth(this.state.controls.email,this.state.controls.password)
+    this.props.onAuth(this.state.controls.email.value,this.state.controls.password.value,this.state.isSignIn)
   }
 
   render() {
@@ -106,6 +111,7 @@ class Auth extends Component {
           {form}
           <Button btnType="Success">Submit</Button>
         </form>
+        <Button clicked={this.switchAuthModeHandler} btnType="Danger">{this.state.isSignIn ? 'Войти' : 'Регистрация'}</Button>
       </div>
     );
   }
@@ -114,7 +120,7 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onAuth:(email,password) => dispatch(actions.auth(email,password))
+    onAuth:(email,password,isSignUp) => dispatch(actions.auth(email,password,isSignUp))
   }
 }
 
--- Myself/react/burgerapp/src/store/actions/auth.js
+++ Myself/react/burgerapp/src/store/actions/auth.js
@@ -1,5 +1,10 @@
 import * as actionTypes from './actionTypes'
+import axios from '../../axios-orders'
+import keys from '../../keys'
 
+// keys should be your unique
+const authLink = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${keys.firebase}`
+const signUpLink = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${keys.firebase}`
 
 export const authStart = () => {
   return {
@@ -21,8 +26,26 @@ export const authFailed = (error) => {
   }
 }
 
-export const auth = (email,password) => {
+export const auth = (email,password,isSignUp) => {
   return dispatch => {
-    dispatch(authStart())
+    dispatch(authStart());
+    const authData = {
+        email: email,
+        password: password,
+        returnSecureToken: true
+    };
+    let url = signUpLink
+    if(isSignUp){
+      url = authLink
+    }
+    axios.post(url, authData)
+    .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+    })
+    .catch(err => {
+        console.log(err);
+        dispatch(authFailed(err));
+    });
   }
 }
