44e2d3b43a1232aecf2f86bf4bf77179a977aa17
Winterstorm <thunpwnz@gmail.com>
2019-05-14 13:25:26 +0400

    Tests : Introduction of Jest and Enzyme

---
+++ Myself/react/burgerapp/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,28 @@
+import { configure , shallow } from 'enzyme'
+import React from 'react'
+import Adapter from 'enzyme-adapter-react-16'
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({
+  adapter:new Adapter()
+})
+
+describe('Navigation Items >',() => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />)
+  })
+
+  it('should render two items if not authenticated',() => {
+    expect(wrapper.find(NavigationItem)).toHaveLength(2)
+  })
+  it('should render two items if authenticated',() => {
+    wrapper.setProps({isAuthenticated:true})
+    expect(wrapper.find(NavigationItem)).toHaveLength(3)
+  })
+  it('should render two items if authenticated',() => {
+    wrapper.setProps({isAuthenticated:true})
+    expect(wrapper.contains(<NavigationItem link="/logout">Log Out</NavigationItem>)).toEqual(true)
+  })
+})
