22733aae1fa57c05157a08d59a0071b0a323c323
Winterstorm <thunpwnz@gmail.com>
2019-05-21 22:13:47 +0400

    Webpack : Academind finished

---
+++ Myself/bundlers/webpack-max/webpack.prod.config.js
@@ -0,0 +1,68 @@
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const Webpack = require('webpack')
+
+module.exports = {
+  devtool:'cheap-module-eval-source-map',
+  entry:'./src/index.js',
+  output: {
+    path:path.resolve(__dirname,'dist'),
+    filename:'bundle.js',
+    chunkFilename:'[id].js',
+    publicPath:'',
+  },
+  resolve: {
+    extensions:['.js','.jsx']
+  },
+  module : {
+    rules: [
+      {
+        test:/\.js$/,
+        loader:'babel-loader',
+        exclude:/node_modules/
+      },
+      {
+        test:/\.css$/,
+        use: [
+          {loader:'style-loader'},
+          {
+            loader:'css-loader',
+            options: {
+              importLoaders: 1,
+              modules:true,
+              localIdentName:'[name]__[local]_[hash:base64:5]'
+            }
+          },
+          {
+            loader:'postcss-loader',
+            options : {
+              ident:'postcss',
+              plugins: () => {
+                autoprefixer({
+                  browsers: [
+                    ">1%",
+                    "last 2 versions"
+                  ]
+                })
+              }
+            }
+          }
+        ],
+        exclude:/node_modules/
+      },
+      {
+        test:/\.(png|jpe?g|gif)$/,
+        loader:'url-loader?limit=8000&name=images/[name].[ext]'
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+      filename:'index.html',
+      inject:'body'
+    }),
+    new Webpack.optimize.UglifyJsPlugin()
+  ]
+}
