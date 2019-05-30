bf241c822d64fdde745ac81f70cc20643a5155da
Winterstorm <thunpwnz@gmail.com>
2019-05-30 21:55:02 +0400

    Code Splitting

---
+++ Myself/react/code-splitting/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Page1 />
+        <Page2 />
+        <Page3 />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Myself/react/code-splitting/src/components/Page1.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+const Page1 = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+      <button onClick={}>Page 2</button>
+      <button onClick={}>Page 3</button>
+    </div>
+  );
+};
+
+export default Page1;
