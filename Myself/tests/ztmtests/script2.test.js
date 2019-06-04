f073d7dc8dc31f42184e91cdbf8f981c22555c3c
Winterstorm <thunpwnz@gmail.com>
2019-06-04 18:37:51 +0400

    Jest Test :  Async and a bit more

---
+++ Myself/tests/ztmtests/script2.test.js
@@ -0,0 +1,37 @@
+const fetch = require('node-fetch')
+
+const swapi = require('./script2')
+
+describe('Async Swapi', () => {
+
+  it('Should return correct value : 87',() => {
+    expect.assertions(1)
+    /*return is important!*/ 
+    return swapi.getPeople(fetch).then(data => {expect(data.count).toEqual(87)})}
+    )
+
+  it('Should work with Promise',() => {
+    expect.assertions(2)
+    return swapi.getPeoplePromise(fetch).then(data => {
+      expect(data.count).toEqual(87)
+      expect(data.results.length).toBeGreaterThan(5)
+    })
+  })
+
+  it('getPeople returns count and results', () => {
+    const mockFetch = jest.fn()
+      .mockReturnValue(Promise.resolve({
+        json:() => Promise.resolve({
+          count:87,
+          results:[0,1,2,3,4,5]
+        })
+      }))
+    expect.assertions(4)
+    return swapi.getPeoplePromise(mockFetch).then(data => {
+      expect(mockFetch.mock.calls.length).toBe(1)
+      expect(mockFetch).toBeCalledWith('https://swapi.co/api/people')
+      expect(data.count).toEqual(87)
+      expect(data.results.length).toBeGreaterThan(5)
+    })}
+    )
+})
