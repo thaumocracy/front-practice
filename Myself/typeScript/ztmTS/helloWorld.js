817228ccb77bdd0fd0e6f2311278eb646f8688fe
Winterstorm <thunpwnz@gmail.com>
2019-06-06 23:34:36 +0400

    TypeScript : Introduction

---
+++ Myself/typeScript/ztmTS/helloWorld.js
@@ -0,0 +1,5 @@
+console.log("Hello world");
+var sum = function (a, b) {
+    return a + b;
+};
+console.log(sum(2, 4));
---
+++ Myself/typeScript/ztmTS/helloWorld.ts
@@ -0,0 +1,7 @@
+console.log(`Hello world`)
+
+const sum = (a:number,b:number) => {
+  return a + b 
+}
+
+console.log(sum(2,4))
