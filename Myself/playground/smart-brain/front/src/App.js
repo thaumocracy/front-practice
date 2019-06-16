a73b2ba37fd43d6fd8627da867f89ea17d2cd4e5
Winterstorm <thunpwnz@gmail.com>
2019-06-16 23:06:27 +0400

    Docker : Working on local and seed

--- Myself/playground/smart-brain/front/src/App.js
+++ Myself/playground/smart-brain/front/src/App.js
@@ -77,7 +77,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('http://192.168.99.100:3001/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -87,7 +87,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('http://192.168.99.100:3001/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- Myself/playground/smart-brain/front/src/components/Register/Register.js
+++ Myself/playground/smart-brain/front/src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('http://192.168.99.100:3001/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
