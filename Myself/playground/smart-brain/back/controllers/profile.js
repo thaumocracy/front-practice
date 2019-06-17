af4e1ed3549470015c6ff39bd3d5531445cda47c
Winterstorm <thunpwnz@gmail.com>
2019-06-18 00:39:46 +0400

    Smartbrain: Profile update

--- Myself/playground/smart-brain/back/controllers/profile.js
+++ Myself/playground/smart-brain/back/controllers/profile.js
@@ -11,6 +11,22 @@ const handleProfileGet = (req, res, db) => {
     .catch(err => res.status(400).json('error getting user'))
 }
 
+const handleProfileUpdate = (req,res,db) => {
+  const {id} = req.params
+  const { name , age , pet } = req.body.formInput
+  db('users')
+    .where({id:id})
+    .update({name:name})
+    .then(response => {
+      if(response){
+        res.json('success')
+      } else {
+        res.status(400).json('Unable to update')
+      }
+    })
+    .catch(error => res.state(400).json(`Error : ${error}`))
+}
 module.exports = {
-  handleProfileGet
+  handleProfileGet,
+  handleProfileUpdate
 }
--- Myself/playground/smart-brain/front/src/components/Profile/Profile.js
+++ Myself/playground/smart-brain/front/src/components/Profile/Profile.js
@@ -3,6 +3,10 @@ import './Profile.css'
 const Profile = ({isProfileOpen , toggleModal}) => {
   return (
     <div className="profile-modal">
+      <img src="http://tachyons.io/img/logo.jpg" className="br-100 pa1 ba b--black-10 h3 w3" alt="avatar"/>
+      <h1>User Name</h1>
+      <h2>Submitted Images : Number</h2>
+      <p>Joined : Date</p>
       <button onClick={toggleModal}>Click to Modal</button>
     </div>
   );
