42c6ccb40e83a14d2a6d369a46cda80c036f2b48
Winterstorm <thunpwnz@gmail.com>
2019-07-15 22:11:10 +0400

    Crown Clothing : Header and Links

--- Myself/react/crown-clothing/src/App.js
+++ Myself/react/crown-clothing/src/App.js
@@ -3,16 +3,16 @@ import './App.scss';
 import {Route , Switch } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage';
 import Shop from './pages/Shop/ShopPage';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch >
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={Shop} />
       </Switch>
-      
-      {/* <HomePage /> */}
     </div>
   );
 }
